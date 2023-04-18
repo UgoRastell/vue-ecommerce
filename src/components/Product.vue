daaaeee25348fdebcc566db414e8c535436e8d29
UgoRastell <urastell@normandiewebschool.fr>
2023-04-18 16:08:13 +0200

    pagination

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="container">
     <h1>Liste des produits</h1>
+    <nav>
+      <ul class="pagination">
+        <li class="page-item" :class="{ disabled: currentPage === 1 }">
+          <button class="page-link" @click="prevPage()">Précédent</button>
+        </li>
+        <li class="page-item" v-for="page in totalPages" :key="page" :class="{ active: currentPage === page }">
+          <button class="page-link" @click="changePage(page)">{{ page }}</button>
+        </li>
+        <li class="page-item" :class="{ disabled: currentPage === totalPages }">
+          <button class="page-link" @click="nextPage()">Suivant</button>
+        </li>
+      </ul>
+    </nav>
     <div class="row">
-      <div v-for="product in productList" :key="product.id" class="col-md-4">
+      <div v-for="product in displayedProducts" :key="product.id" class="col-md-4">
         <div class="card">
           <div class="card-img-container">
             <img :src="product.image" alt="Product Image" class="card-img-top">
@@ -10,7 +23,7 @@
           <div class="card-body">
             <h2 class="card-title">{{ product.title }}</h2>
             <p class="card-text">Prix : {{ product.price }} €</p>
-            <button class="btn btn-primary" @click="getProduct(product.id)">Voir le produit</button>
+            <button class="btn btn-primary" @click="viewProduct(product.id)">Voir le produit</button>
           </div>
         </div>
       </div>
@@ -18,33 +31,65 @@
   </div>
 </template>
 
-
-
 <script>
 import { useProductStore } from '@/stores/products'
+import * as Vue from 'vue'
+import { useRouter } from 'vue-router'
 
 export default {
   name: 'ProductList',
   setup() {
     const productStore = useProductStore()
-    console.log(productStore)
+    const currentPage = Vue.ref(1)
+    const productsPerPage = 6
+    const router = useRouter()
+
+    const displayedProducts = Vue.computed(() => {
+      const startIndex = (currentPage.value - 1) * productsPerPage
+      const endIndex = startIndex + productsPerPage
+      return productStore.getProducts.slice(startIndex, endIndex)
+    })
+
+    const totalPages = Vue.computed(() => {
+      return Math.ceil(productStore.getProducts.length / productsPerPage)
+    })
+
+    const viewProduct = async (id) => {
+      await productStore.viewProduct(id)
+      router.push({ name: 'SingleProduct', params: { id }})
+    }
+
+    const prevPage = () => {
+      if (currentPage.value > 1) {
+        currentPage.value--
+      }
+    }
+
+    const nextPage = () => {
+      if (currentPage.value < totalPages.value) {
+        currentPage.value++
+      }
+    }
+
+    const changePage = (page) => {
+      currentPage.value = page
+    }
+
+    Vue.onMounted(async () => {
+      await productStore.fetchProducts()
+    })
+
     return {
-      productStore,
+      displayedProducts,
+      currentPage,
+      totalPages,
+      prevPage,
+      nextPage,
+      changePage,
+      viewProduct
     }
   },
-  data(){
-      return {productList:[]}
-  },
-  async mounted(){
-      await this.productStore.fetchProducts()
-      this.productList = this.productStore.getProducts
-  },
-  methods: {
-    async getProduct(id){
-      await this.productStore.viewProduct(id)
-      console.log(id)
-      this.$router.push({ name: 'SingleProduct', params: { id }})
-    }
-  }
 }
+
 </script>
+
